a429ce068ff2ce2a5ed4bd6e62e9c0d144daae28
Warren Ayivor <warrenayiss@outlook.com>
2021-01-22 21:08:47 +0000

    Implementation of the update and delete feature

--- WFinanceApp/TransactionDetailsWindow.xaml.cs
+++ WFinanceApp/TransactionDetailsWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -24,6 +25,14 @@ namespace WFinanceApp
 			InitializeComponent();
 
 			this.transaction = transaction;
+			descriptionInput.Text = transaction.Description;
+			amountInput.Text = transaction.Amount.ToString();
+
+		}
+
+		public float AmountTrans(string value)
+		{
+			return float.Parse(value, CultureInfo.InvariantCulture.NumberFormat);
 		}
 
 		private void UpdateBtn_Click(object sender, RoutedEventArgs e)
@@ -32,6 +41,16 @@ namespace WFinanceApp
 			//Require: click on update button
 			//Modify: Close the window
 			//Effect: Change the values of the selected transaction in the database
+			transaction.Description = descriptionInput.Text;
+			transaction.Amount = AmountTrans(amountInput.Text);
+			transaction.Date = transaction.Date;
+
+			using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+			{
+				connection.CreateTable<Transaction>();
+				connection.Update(transaction);
+			}
+			Close();
 		}
 
 		private void DeleteBtn_Click(object sender, RoutedEventArgs e)
@@ -40,6 +59,12 @@ namespace WFinanceApp
 			//Require: click on delete button
 			//Modify: Close the window
 			//Effect: Delete element of the database
+			using (SQLite.SQLiteConnection connection = new SQLite.SQLiteConnection(App.databasePath))
+			{
+				connection.CreateTable<Transaction>();
+				connection.Delete(transaction);
+			}
+			Close();
 		}
 	}
 }
